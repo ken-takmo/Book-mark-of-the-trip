33df244138629f24d3fd65a25180cbf7337e8f33
kenichi <xianyigoben53@gmail.com>
2022-07-26 19:04:00 +0900

    投稿時バリエテーション

---
+++ trip_create.php
@@ -0,0 +1,48 @@
+<?php
+
+    require_once("Database.php");
+
+    use Trip\Database;
+
+    $trips = $_POST; 
+
+    if(empty($trips['destination'])){
+        exit('旅行先を入力してください');
+    }else if(mb_strlen($trips['destination']) > 100){
+        exit('旅行先は100文字以内にしてください');
+    }
+    if(empty($trips['content'])){
+        exit('感想を入力してください');
+    }
+    if(empty($trips['evaluation'])){
+        exit('評価を選択してください');
+    }
+    if(empty($trips['companion'])){
+        exit('公開ステータスを選択してください');
+    }
+    if(empty($trips['tripDate'])){
+        exit('旅行した日を選択してください');
+    }
+
+    $dbh = Database::dbConnect();
+    $dbh->beginTransaction();
+    $sql = 'INSERT INTO 
+                trip_app(destination, content, evaluation, companion, tripDate)
+            VALUES
+                (:destination, :content, :evaluation, :companion, :tripDate)';
+    try{
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue(':destination', $trips['destination'], PDO::PARAM_STR);
+        $stmt->bindValue(':content', $trips['content'], PDO::PARAM_STR);
+        $stmt->bindValue(':evaluation', $trips['evaluation'], PDO::PARAM_INT);
+        $stmt->bindValue(':companion', $trips['companion'], PDO::PARAM_INT);
+        $stmt->bindValue(':tripDate', $trips['tripDate'], PDO::PARAM_STR);
+        $stmt->execute();
+        $dbh->commit();
+        echo '投稿されました';
+    }catch(PDOException $e){
+        $dbh->rollBack();
+        exit($e);
+    }
+?>
+<p><a href="/">戻る</a></p>
