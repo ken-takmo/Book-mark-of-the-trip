9ad9c425e489a87f8a35da6d7125f00fb7c8a698
kenichi <xianyigoben53@gmail.com>
2022-07-27 15:03:26 +0900

    detaildeletefunction detailfunction setCompanionfunctionの追加ページの作成

--- functions.php
+++ functions.php
@@ -9,9 +9,60 @@
     {   
         public static function getAll() {
             $dbh = Database::dbConnect();
-            $sql = "SELECT * FROM trip_app";
-            $stmt = $dbh->query($sql);
+            $stmt = $dbh->query("SELECT * FROM trip_app");
             $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
             return $result;
+        }
+
+        public static function getDetail($id) {
+            if(empty($id)) {
+                echo "IDが不正です";
+            }
+            $dbh = Database::dbConnect();
+            $stmt = $dbh->prepare("SELECT * FROM trip_app WHERE id = :id");
+            $stmt->bindValue(':id', (int)$id, \PDO::PARAM_INT);
+            $stmt->execute();
+            $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+            if(!$result){
+                echo "その投稿はありません";
+            }
+            return $result;
+        }
+
+        public static function deleteTrip($id) {
+            if(empty($id)) {
+                echo "IDが不正です";
+            }
+            $dbh = Database::dbConnect();
+            $dbh->beginTransaction();
+            $sql = 'DELETE FROM trip_app WHERE id = :id';
+            try{
+                $stmt = $dbh->prepare($sql);
+                $stmt->bindValue(':id', (int)$id, \PDO::PARAM_INT);
+                $stmt->execute();
+                $dbh->commit();
+                echo '削除されました';
+            }catch(PDOException $e){
+                $dbh->rollBack();
+                exit($e);
+            }
+        }
+
+        public static function setCompanion($int){
+            switch($int){
+                case 1:
+                    return "ひとり";
+                    break;
+                case 2:
+                    return "友人";
+                    break;
+                case 3:
+                    return "恋人・パートナー";
+                    break;
+                case 4:
+                    return "家族";
+                    break;
+            }
         }
     }
