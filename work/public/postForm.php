bc8977b800c6558d4019cbad9deb93a322bf5164
kenichi <xianyigoben53@gmail.com>
2022-07-29 03:17:08 +0900

    postformとsearchformのスタイルへんこう

--- work/public/postForm.php
+++ work/public/postForm.php
@@ -17,35 +17,37 @@
     <?php include("../app/header.php"); ?>
     <h1>投稿フォーム</h1>
     <form action="../app/trip_create.php" method="POST">
-      <p>旅行先</p>
-      <input type="text" name="destination" />
-      <p>旅行テーマ</p>
-      <textarea name="theme" cols="30" rows="10"></textarea>
-      <p>感想</p>
-      <textarea name="content" cols="30" rows="10"></textarea>
-      <p>評価(5段階評価)</p>
-      <select name="evaluation">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-      <p>誰と</p>
-      <select name="companion">
-        <option value="1">ひとり</option>
-        <option value="2">友人</option>
-        <option value="3">恋人・パートナー</option>
-        <option value="4">家族</option>
-      </select>
-      <p>旅行日</p>
-      <input name="tripDate" type="date" />
-      <p>地域</p>
-      <select name="region">
-        <?php for($i = 0; $i <= 46; $i++): ?>
-          <option value="<?= $i + 1 ?>"><?= $regions[$i] ?></option>
-        <?php endfor; ?>
-      </select>
+      <label for="destination">旅行先</label>
+      <input type="text" name="destination" id="destination"/>
+      <label for="theme">旅行テーマ</label>
+      <textarea name="theme" id="theme" cols="30" rows="10"></textarea>
+      <label for="content">感想</label>
+      <textarea name="content" id="content" cols="30" rows="10"></textarea>
+      <div class="selecter">
+        <label for="evaluation">評価</label>
+        <select name="evaluation" id="evaluation">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <label for="companion">同行者</label>
+        <select name="companion" id="companion">
+          <option value="1">ひとり</option>
+          <option value="2">友人</option>
+          <option value="3">恋人・パートナー</option>
+          <option value="4">家族</option>
+        </select>
+        <label for="tripDate">旅行日(入力も可)</label>
+        <input name="tripDate" id="tripDate" type="date" />
+        <label for="region">地域</label>
+        <select name="region" id="region">
+          <?php for($i = 0; $i <= 46; $i++): ?>
+            <option value="<?= $i + 1 ?>"><?= $regions[$i] ?></option>
+          <?php endfor; ?>
+        </select>
+      </div>
       <br />
       <br />
       <input type="submit" value="投稿" />
--- work/public/search_form.php
+++ work/public/search_form.php
@@ -11,23 +11,27 @@
     <?php include("../app/header.php"); ?>
     <h1>検索フォーム</h1>
     <form action="./search.php" method="POST">
-      <p>場所</p>
-      <input type="text" name="destination" />
-      <p>評価</p>
-      <select name="evaluation">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-      </select>
-      <p>誰と</p>
-      <select name="companion">
-        <option value="1">ひとり</option>
-        <option value="2">友人</option>
-        <option value="3">恋人・パートナー</option>
-        <option value="4">家族</option>
-      </select>
+      <label for="destination">旅行先</label>
+      <input type="text" name="destination" id="destination"/>
+      <div class="selecter">
+        <label for="evaluation">評価</label>
+        <select name="evaluation" id="evaluation">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <label for="companion">同行者</label>
+        <select name="companion" id="companion">
+          <option value="1">ひとり</option>
+          <option value="2">友人</option>
+          <option value="3">恋人・パートナー</option>
+          <option value="4">家族</option>
+        </select>
+      </div>
+      <br>
+      <br>
       <input type="submit" value="検索" />
     </form>
   </body>
